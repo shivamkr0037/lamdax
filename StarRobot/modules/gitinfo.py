5dc6589b75561e577b819a856631000762464c00
sᴛᴀʀ ʙᴏɪ <124703719+itz-star-boi@users.noreply.github.com>
2023-05-06 15:56:47 +0530

    Update gitinfo.py

--- StarRobot/modules/gitinfo.py
+++ StarRobot/modules/gitinfo.py
@@ -45,7 +45,7 @@ async def github(_, message):
     await message.reply_photo(photo=avatar_url, caption=caption)
 
 
-__mod_name__ = "✨Gɪᴛʜᴜʙ✨"
+__mod_name__ = "🌹Gɪᴛʜᴜʙ🌹"
 
 __help__ = """
 ɪ ᴡɪʟʟ ɢɪᴠᴇ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴀʙᴏᴜᴛ ɢɪᴛʜᴜʙ ᴘʀᴏғɪʟᴇ 
