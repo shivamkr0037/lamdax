fa27078549fb783fd46bfd1c93ecba1f50af170b
sᴛᴀʀ ʙᴏɪ <124703719+itz-star-boi@users.noreply.github.com>
2023-05-06 15:41:02 +0530

    Update bans.py

--- StarRobot/modules/bans.py
+++ StarRobot/modules/bans.py
@@ -426,7 +426,7 @@ dispatcher.add_handler(UNBAN_HANDLER)
 dispatcher.add_handler(ROAR_HANDLER)
 dispatcher.add_handler(KICKME_HANDLER)
 
-__mod_name__ = "🌹️ʙᴀɴ🌹"
+__mod_name__ = "⚡ʙᴀɴ⚡"
 __handlers__ = [
     BAN_HANDLER,
     TEMPBAN_HANDLER,
