fec85bd593aa768d3d1bdd5c92f1e30b215a3107
sᴛᴀʀ ʙᴏɪ <124703719+itz-star-boi@users.noreply.github.com>
2023-05-06 15:39:09 +0530

    Update animez.py

--- StarRobot/modules/animez.py
+++ StarRobot/modules/animez.py
@@ -574,7 +574,7 @@ dispatcher.add_handler(KAIZOKU_SEARCH_HANDLER)
 dispatcher.add_handler(KAYO_SEARCH_HANDLER)
 dispatcher.add_handler(UPCOMING_HANDLER)
 
-__mod_name__ = "🌹️Aɴɪᴍᴇ🌹️"
+__mod_name__ = "⚡Aɴɪᴍᴇ⚡"
 __command_list__ = [
     "anime",
     "manga",
