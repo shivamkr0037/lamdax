64cda00239b51daa71872cb0e89e05d32cbaecba
sᴛᴀʀ ʙᴏɪ <124703719+itz-star-boi@users.noreply.github.com>
2023-05-06 16:42:16 +0530

    Update translator.py

--- StarRobot/modules/translator.py
+++ StarRobot/modules/translator.py
@@ -60,7 +60,7 @@ sm,sn,so,sq,sr,st,su,sv,sw,ta,te,tg,th,tl,tr,uk,ur,uz,
 vi,xh,yi,yo,zh,zh_CN,zh_TW,zu`
 
 """
-__mod_name__ = "⚡Tʀᴀɴs⚡"
+__mod_name__ = "✨Tʀᴀɴs✨"
 
 TRANSLATE_HANDLER = DisableAbleCommandHandler(["tr", "tl"], totranslate)
 
