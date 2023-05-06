f0d3513932cf905cb838683ce0f4f1b1b3d0f07e
sᴛᴀʀ ʙᴏɪ <124703719+itz-star-boi@users.noreply.github.com>
2023-05-06 16:05:40 +0530

    Update phone.py

--- StarRobot/modules/phone.py
+++ StarRobot/modules/phone.py
@@ -7,7 +7,7 @@ from StarRobot import dispatcher
 from StarRobot.modules.helper_funcs.alternate import send_message
 from StarRobot.modules.helper_funcs.chat_status import user_admin
 
-__mod_name__ = "✨𝙿ʜᴏɴᴇ✨"
+__mod_name__ = "🌹𝙿ʜᴏɴᴇ🌹"
 __help__ = """
 » /phone ꜰɪʟʟ ᴀɴʏ ᴍᴏʙɪʟᴇ ɴᴜᴍʙᴇʀ ᴛᴏ ᴄʜᴇᴄᴋ ɪɴꜰᴏ.
 
