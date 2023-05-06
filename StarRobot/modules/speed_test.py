20f7de7dc045b7a5967fa09f8ec74873ec285669
sᴛᴀʀ ʙᴏɪ <124703719+itz-star-boi@users.noreply.github.com>
2023-05-06 16:45:04 +0530

    Update speed_test.py

--- StarRobot/modules/speed_test.py
+++ StarRobot/modules/speed_test.py
@@ -65,6 +65,6 @@ __help__ = """
 
 """
 
-__mod_name__ = "⚡SᴘᴇᴇᴅTᴇsᴛ⚡​"
+__mod_name__ = "✨SᴘᴇᴇᴅTᴇsᴛ✨​"
 __command_list__ = ["speedtest"]
 __handlers__ = [SPEED_TEST_HANDLER, SPEED_TEST_CALLBACKHANDLER]
