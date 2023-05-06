8a00f5eabbad77148a0cd54466af7959f8ff95fb
sᴛᴀʀ ʙᴏɪ <124703719+itz-star-boi@users.noreply.github.com>
2023-05-06 15:38:39 +0530

    Update anime.py

--- StarRobot/modules/anime.py
+++ StarRobot/modules/anime.py
@@ -139,7 +139,7 @@ dispatcher.add_handler(QUOTE_CHANGE)
 dispatcher.add_handler(ANIMEQUOTES_HANDLER)
 dispatcher.add_handler(QUOTES_HANDLER)
 
-__mod_name__ = "🌹️ǫᴜᴏᴛᴇs🌹️"
+__mod_name__ = "⚡ǫᴜᴏᴛᴇs⚡"
 __help__ = """
 
 /quote :- ᴡʀɪᴛᴇ ǫᴜᴏᴛᴇs
