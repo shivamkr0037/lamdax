fe44f10f66b5f886bb686d8357ba380738d9e82f
sᴛᴀʀ ʙᴏɪ <124703719+itz-star-boi@users.noreply.github.com>
2023-05-06 16:00:14 +0530

    Update logo.py

--- StarRobot/modules/logo.py
+++ StarRobot/modules/logo.py
@@ -319,7 +319,7 @@ async def lego(event):
             os.remove(fname)
 
 
-__mod_name__ = "✨ʟᴏɢᴏ✨"
+__mod_name__ = "🌹ʟᴏɢᴏ🌹"
 
 __help__ = """
 I can create some beautiful and attractive logo for your profile pics.
