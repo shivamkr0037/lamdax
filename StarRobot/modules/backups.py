3492cb9f7c506b656b10f20a327ce5396bfb0102
sᴛᴀʀ ʙᴏɪ <124703719+itz-star-boi@users.noreply.github.com>
2023-05-06 15:40:36 +0530

    Update backups.py

--- StarRobot/modules/backups.py
+++ StarRobot/modules/backups.py
@@ -368,7 +368,7 @@ def get_chat(chat_id, chat_data):
         return {"status": False, "value": False}
 
 
-__mod_name__ = "🌹️Bᴀᴄᴋᴜᴘ🌹"
+__mod_name__ = "⚡Bᴀᴄᴋᴜᴘ⚡"
 
 __help__ = """
 *ᴏɴʟʏ ғᴏʀ ɢʀᴏᴜᴘ ᴏᴡɴᴇʀ:*
