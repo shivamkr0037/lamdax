a564f1ad8a04450709b088c56378dc0469d64a7b
sᴛᴀʀ ʙᴏɪ <124703719+itz-star-boi@users.noreply.github.com>
2023-05-06 15:36:39 +0530

    Update afk.py

--- StarRobot/modules/afk.py
+++ StarRobot/modules/afk.py
@@ -163,7 +163,7 @@ dispatcher.add_handler(AFK_REGEX_HANDLER, AFK_GROUP)
 dispatcher.add_handler(NO_AFK_HANDLER, AFK_GROUP)
 dispatcher.add_handler(AFK_REPLY_HANDLER, AFK_REPLY_GROUP)
 
-__mod_name__ = "🌹Aꜰᴋ🌹"
+__mod_name__ = "⚡Aꜰᴋ⚡"
 __command_list__ = ["afk"]
 __handlers__ = [
     (AFK_HANDLER, AFK_GROUP),
